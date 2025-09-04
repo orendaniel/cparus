40204d3dfd85ff6cd41d6097ff6d64d05c7d113b
orendaniel <orendaniel150@gmail.com>
2025-09-04 21:17:18 +0300

    fixed examples

--- src/parus_predefined.c
+++ src/parus_predefined.c
@@ -696,6 +696,9 @@ static int seqterm(void* stk, void* lex) {
 	return 0;
 }
 
+// PREDEFINED LEXICON
+// ----------------------------------------------------------------------------------------------------
+
 Lexicon* predefined_lexicon() {
 	Lexicon* lex = make_lexicon();
 
@@ -736,8 +739,8 @@ Lexicon* predefined_lexicon() {
 	lexicon_define(lex, "dpl", make_parus_baseop(&dpl));
 	lexicon_define(lex, "setat", make_parus_baseop(&setat));
 	lexicon_define(lex, "for", make_parus_baseop(&for_op));
-	lexicon_define(lex, "case", make_parus_quote(make_parus_symbol("case")));
-	lexicon_define(lex, "end-case", make_parus_baseop(&end_case_op));
+	//lexicon_define(lex, "case", make_parus_quote(make_parus_symbol("case")));
+	//lexicon_define(lex, "end-case", make_parus_baseop(&end_case_op));
 	lexicon_define(lex, "quit", make_parus_baseop(&quit));
 
 	lexicon_define(lex, "?stk", make_parus_baseop(&stkprint));
